c7e59f4935182d022cd79751647fc6ca0f2b4890
HenriqueSPiana <h.spiana80@gmail.com>
2024-11-03 11:14:25 -0300

    Inclui matricula e dados de aluno para avaliacao

--- test.py
+++ test.py
@@ -2,10 +2,10 @@
 # Preencha seus dados e leia a declaração de honestidade abaixo. NÃO APAGUE
 # nenhuma linha deste comentário de seu código!
 #
-#    Nome completo:
-#    Matrícula:
-#    Turma:
-#    Email:
+#    Nome completo:Henrique Schrock Piana
+#    Matrícula:202202403
+#    Turma:CC6N
+#    Email:h.spiana80@gmail.com
 #
 # DECLARAÇÃO DE HONESTIDADE ACADÊMICA:
 # Eu afirmo que o código abaixo foi de minha autoria. Também afirmo que não
